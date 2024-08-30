bedb98a1cdca6b82f6aa65010359b40b8e062065
Rocklessgamer <167827536+Rocklessgamer@users.noreply.github.com>
2024-08-29 20:10:00 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const formContainer = document.getElementById('form-container');
-    if (formContainer) {
-        formContainer.innerHTML = "<p>JavaScript is working!</p>";
-    } else {
-        console.log("Form container not found.");
-    }
-});
